80926b34e81db28fc9486530649109cb8e6f3c3e
Marcin Polak <mapoart@gmail.com>
2020-09-18 00:54:29 +0200

    Added support for Ubuntu 20.04

--- racket.linux.nexss.config.js
+++ racket.linux.nexss.config.js
@@ -5,7 +5,7 @@ if (process.getuid && process.getuid() === 0) {
 }
 languageConfig.compilers = {
   racket: {
-    install: `${sudo}add-apt-repository -y ppa:plt/racket && ${sudo}apt update && ${sudo}apt install -y racket`,
+    install: `${sudo}apt install -y software-properties-common && add-apt-repository -y ppa:plt/racket && ${sudo}apt update && ${sudo}apt install -y racket`,
     command: "racket",
     args: "-l errortrace -t <file> --",
     help: ``,
